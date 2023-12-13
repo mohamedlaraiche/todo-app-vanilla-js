b624ad017444ec817790007f27f86edd9e891ca2
mohamedlaraiche <laraichemohamed@gmail.com>
2023-12-13 18:37:31 +0100

    updates: finish the script

--- js/script.js
+++ js/script.js
@@ -1,53 +1,139 @@
 // Set classes
-
 const createInput = document.querySelector('.createInput');
-const createBtn = document.querySelector('.createBtn');
 const todosHolder = document.querySelector('.todosHolder');
 const form = document.querySelector('.formInput');
 
-// Create elements
-
 // Functions
+const saveTodosToLocalStorage = (todos) => {
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+const getTodosFromLocalStorage = () => {
+  const storedTodos = localStorage.getItem('todos');
+  return storedTodos ? JSON.parse(storedTodos) : [];
+};
+
+const updateTodoInLocalStorage = (id, newText) => {
+  const todos = getTodosFromLocalStorage();
+  const updatedTodos = todos.map((todo) => {
+    if (todo.id === id) {
+      return { ...todo, text: newText };
+    }
+    return todo;
+  });
+  saveTodosToLocalStorage(updatedTodos);
+};
+
+const displayTodo = (todo) => {
+  const division = document.createElement('div');
+  division.className = 'todo';
+  division.id = 'todo-' + todo.id;
+
+  const paragraph = document.createElement('p');
+  paragraph.className = 'todoText';
+  paragraph.textContent = todo.text;
+
+  const input = document.createElement('input');
+  input.className = 'editInput hide';
+  input.type = 'text';
+  input.value = todo.text;
+
+  const division2 = document.createElement('div');
+  division2.className = 'mngBtns';
+
+  const buttonUpdate = document.createElement('button');
+  buttonUpdate.className = 'updateBtn';
+  buttonUpdate.onclick = function () {
+    toggleEdit(todo.id);
+  };
+
+  const updateIcon = document.createElement('i');
+  updateIcon.className = 'fa-solid fa-pen-to-square';
+
+  const buttonSave = document.createElement('button');
+  buttonSave.className = 'saveBtn hide';
+  buttonSave.textContent = 'Save';
+  buttonSave.onclick = function () {
+    saveUpdatedTodo(todo.id);
+  };
+
+  const buttonDelete = document.createElement('button');
+  buttonDelete.className = 'deleteBtn';
+  buttonDelete.onclick = function () {
+    deleteTodo(todo.id);
+  };
+
+  const deleteIcon = document.createElement('i');
+  deleteIcon.className = 'fa-solid fa-trash-can';
+
+  division.appendChild(paragraph);
+  division.appendChild(input);
+  division.appendChild(division2);
+  buttonUpdate.appendChild(updateIcon);
+  buttonDelete.appendChild(deleteIcon);
+  division2.appendChild(buttonUpdate);
+  division2.appendChild(buttonSave);
+  division2.appendChild(buttonDelete);
+
+  todosHolder.appendChild(division);
+};
+
 const createTodo = (e) => {
   e.preventDefault();
   if (!createInput.value) {
     console.log('please fill the field');
   } else {
-    const division = document.createElement('div');
-    const input = document.createElement('input');
-    division.className = 'todo';
-    const paragraph = document.createElement('p');
-    paragraph.className = 'todoText show';
-    paragraph.textContent = createInput.value;
-    input.className = ' editInput hide';
-    const division2 = document.createElement('div');
-
-    division2.className = 'mngBtns';
-    const buttonUpdate = document.createElement('button');
-    buttonUpdate.className = 'updateBtn';
-    const updateIcon = document.createElement('i');
-
-    updateIcon.className = 'fa-solid fa-pen-to-square';
-    const buttonDelete = document.createElement('button');
-    buttonDelete.className = 'deleteBtn';
-    const deleteIcon = document.createElement('i');
-
-    deleteIcon.className = 'fa-solid fa-trash-can';
-
-    division.appendChild(paragraph);
-    division.appendChild(input);
-    division.appendChild(division2);
-    buttonUpdate.appendChild(updateIcon);
-    buttonDelete.appendChild(deleteIcon);
-    division2.appendChild(buttonUpdate);
-    division2.appendChild(buttonDelete);
-
-    todosHolder.appendChild(division);
-
-    localStorage.setItem('todo', division);
+    const newTodo = { id: Date.now(), text: createInput.value };
+    const todos = getTodosFromLocalStorage();
+    todos.push(newTodo);
+    saveTodosToLocalStorage(todos);
+    displayTodo(newTodo);
+    createInput.value = ''; // Clear the input after adding
   }
 };
 
-console.log(todosHolder.value === undefined);
+const deleteTodo = (id) => {
+  const todos = getTodosFromLocalStorage();
+  const updatedTodos = todos.filter((todo) => todo.id !== id);
+  saveTodosToLocalStorage(updatedTodos);
+
+  const todoElement = document.getElementById('todo-' + id);
+  if (todoElement) {
+    todosHolder.removeChild(todoElement);
+  }
+};
+
+const toggleEdit = (id) => {
+  const todoElement = document.getElementById('todo-' + id);
+  const paragraph = todoElement.querySelector('.todoText');
+  const input = todoElement.querySelector('.editInput');
+  const updateBtn = todoElement.querySelector('.updateBtn');
+  const saveBtn = todoElement.querySelector('.saveBtn');
+
+  paragraph.classList.toggle('hide');
+  input.classList.toggle('hide');
+  updateBtn.classList.toggle('hide');
+  saveBtn.classList.toggle('hide');
+
+  input.focus();
+};
+
+const saveUpdatedTodo = (id) => {
+  const todoElement = document.getElementById('todo-' + id);
+  const input = todoElement.querySelector('.editInput');
+
+  updateTodoInLocalStorage(id, input.value);
+  toggleEdit(id);
+
+  const paragraph = todoElement.querySelector('.todoText');
+  paragraph.textContent = input.value;
+};
+
+// Load existing todos on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const todos = getTodosFromLocalStorage();
+  todos.forEach((todo) => displayTodo(todo));
+});
+
 // Events
 form.addEventListener('submit', createTodo);
